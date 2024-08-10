2cf49a6801df42e75e100b892b53cdaa6eb9e10b
Paula Cano <paulacanoroman@gmail.com>
2024-08-10 11:05:48 +0200

    feat:. login

--- mi-app/src/components/Login/Login.tsx
+++ mi-app/src/components/Login/Login.tsx
@@ -2,17 +2,34 @@ import React from 'react'
 import { useForm } from 'react-hook-form'
 import styleLogin from './Login.module.css'
 
+interface FormState {
+    user: string;
+    pass: string;
+}
+
 const Login = () => {
 
-    const { register, handleSubmit } = useForm()
+    const { register, handleSubmit, formState: { errors }  } = useForm<FormState>()
+
+    const onSubmit = (data:FormState)=>{
+        console.log(data)
+    }
     
   return (
     <div>
-        <form className={styleLogin.form}>
+        <form className={styleLogin.form} onSubmit={handleSubmit(onSubmit)}>
             <label>User</label>
-            <input placeholder='Write your user name' type='text'></input>
+            <input placeholder='Write your user name' type='text' {...register('user', { required: 'El nombre de usuario es requerido' })}></input>
             <label>Password</label>
-            <input placeholder='Write your password' type='text'></input>
+            <input placeholder='Write your password' type='text'  {...register("pass", { required: {
+                      value: true,
+                      message: "El campo es requerido"
+                    }})}>
+                        
+             </input>
+             
+             {errors.pass && <p>{errors.pass.message}</p>}
+             
             <input type='submit'></input>
         </form>
     </div>
